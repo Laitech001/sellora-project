6b52f6294d5de049fecce998931c82c93f3dc19b
Abdulganiy Ibrahim <abdulgaiyibrahim095@gmail.com>
2026-06-15 05:05:40 +0100

    IMPROVEMENT: Improve store page Hero styling and display the store name in hero section

--- app/store/[slug]/page.tsx
+++ app/store/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "@/components/store/Hero";
 import StoreClient from "@/components/store/StoreClient";
 import { getProductsBySlug } from "@/lib/data/Products";
+import { getStoreBySlug } from "@/lib/data/store";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: Promise<{
@@ -11,10 +13,15 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { slug } = await params;
   const products = await getProductsBySlug(slug);
+  const store = await getStoreBySlug(slug);
+
+  if (!store) {
+    notFound();
+  }
 
   return (
     <>
-      <Hero />
+      <Hero store={store}/>
 
       <StoreClient products={products} slug={slug} />
 
--- components/store/Hero.tsx
+++ components/store/Hero.tsx
@@ -1,17 +1,25 @@
+type heroProps = {
+  store: {
+    id: string;
+    name: string;
+    slug: string;
+  }
+}
 
-export default function Hero() {
+export default function Hero({ store }: heroProps) {
   return (
-    <div className="w-full py-2 flex items-center justify-center rounded-lg bg-gray-100">
+    <div className="w-full py-2 flex flex-col items-center justify-center rounded-lg bg-linear-to-r from-sky-100 via-40% to-primary-200">
 
-      <section className="px-4 bg-gray-50 bg-linear-to-b from-gray-50 to-white rounded">
-        <h1 className="text-center text-4xl font-bold mb-4">
-          Welcome to Sellora
+        <div className="bg-primary-500/20 flex justify-center items-center px-4 py-1 rounded-full">
+          <p className="text-primary-500 text-md font-semibold">
+            {`Welcome to ${store.name} store`}
+          </p>
+        </div>
+
+        <h1 className="text-center text-gray-800 text-xl md:text-2xl lg:text-4xl font-semibold max-w-xl mx-auto">
+          Discover the best <br></br> products at the <span className="bg-linear-to-r from-sky-500 to-primary-500 bg-clip-text text-transparent">lowest prices</span>
         </h1>
 
-        <p className="text-center text-gray-600 text-lg max-w-xl mx-auto">
-          Discover the best products at the lowest prices
-        </p>
-      </section>
 
     </div>
   )
